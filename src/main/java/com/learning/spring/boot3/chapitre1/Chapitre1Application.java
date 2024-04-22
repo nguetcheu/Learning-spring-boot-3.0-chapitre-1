a186e92d9ca69c6904b07daea34721e975b54594
nguetcheu <dnguetcheu@gmail.com>
2024-04-22 21:55:27 +0200

    Création du projet

---
+++ src/main/java/com/learning/spring/boot3/chapitre1/Chapitre1Application.java
@@ -0,0 +1,13 @@
+package com.learning.spring.boot3.chapitre1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapitre1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapitre1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/spring/boot3/chapitre1/Chapitre1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.boot3.chapitre1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapitre1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
